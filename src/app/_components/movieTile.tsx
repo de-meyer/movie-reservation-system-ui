f6c5b19bf384780520bc7a6066b2de8db3e26570
de-meyer <dmeyer.private@gmail.com>
2025-05-03 23:45:45 +0200

    make it look nice

--- src/app/_components/movieTile.tsx
+++ src/app/_components/movieTile.tsx
@@ -7,16 +7,16 @@ type MovieTileProps = {
 export default function MovieTile({ title, url, image }: MovieTileProps) {
   const preFix = "data:image/png;base64,";
   return (
-    <div className="border-2 border-gray-800 rounded-lg p-4 bg-gray-900">
+    <div className="border-2 w-full border-gray-800 rounded-lg pb-4 bg-gray-900">
       <Image
         src={`${preFix}${image}`}
         alt={title}
+        layout="responsive"
         width={200}
         height={300}
         className="rounded-lg mb-2"
       />
       <p>{title}</p>
-      <p>{url}</p>
     </div>
   );
 }
