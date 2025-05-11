3b5faa739cbefb7a1689e8c49773c2180c78bce9
de-meyer <dmeyer.private@gmail.com>
2025-05-11 22:23:22 +0200

    get movie information

--- src/app/_components/movieTile.tsx
+++ src/app/_components/movieTile.tsx
@@ -1,4 +1,6 @@
+import axios from "axios";
 import Image from "next/image";
+import { useState } from "react";
 type MovieTileProps = {
   title: string;
   url: string;
@@ -6,16 +8,45 @@ type MovieTileProps = {
 };
 export default function MovieTile({ title, url, image }: MovieTileProps) {
   const preFix = "data:image/png;base64,";
+  const [isHovered, setIsHovered] = useState(false);
+  function getMovieInfo(url: string): void {
+    axios
+      .get(`http://localhost:8080/` + url)
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+
   return (
-    <div className="w-full border-gray-800 rounded-lg  bg-gray-900">
+    <div
+      className={`relative w-full  rounded-lg  overflow-hidden transition-transform duration-300 ease-in-out ${
+        isHovered
+          ? "scale-150 z-10 border-gray-800 bg-gray-900"
+          : "scale-100 z-0"
+      }`}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      onClick={() => getMovieInfo(url)}>
+      {/* Movie Image */}
       <Image
         src={`${preFix}${image}`}
         alt={title}
-        layout="responsive"
         width={200}
         height={300}
-        className="rounded-lg hover:scale-120 transition-transform duration-300 ease-in-out"
+        className="rounded-lg"
       />
+
+      {/* Movie Title (Only Visible on Hover) */}
+      {isHovered && (
+        <div className="left-0 w-full bg-black  opacity-75 py-2 z-10">
+          <p className="text-white text-center text-sm font-semibold ">
+            {title}
+          </p>
+        </div>
+      )}
     </div>
   );
 }
