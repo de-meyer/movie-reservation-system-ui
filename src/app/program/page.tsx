8ef87cdb4067ddf729d1c6527c0ed068df4192e9
de-meyer <dmeyer.private@gmail.com>
2025-11-02 02:11:59 +0100

    feat: fetch Program data

--- src/app/program/page.tsx
+++ src/app/program/page.tsx
@@ -1,4 +1,21 @@
+"use client";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 export default function Program() {
+  const [program, setProgram] = useState<any[]>([]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:8080/program/current")
+      .then((res) => {
+        setProgram(res.data);
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+
   return (
     <main className="flex min-h-[calc(100vh-var(--header-height))] flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
