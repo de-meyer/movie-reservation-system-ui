911e556ef5907b3ef035c0b3aebc43122c149e2f
de-meyer <dmeyer.private@gmail.com>
2025-11-22 19:34:37 +0100

    chore: clean up code

--- src/app/_components/progressBar.tsx
+++ src/app/_components/progressBar.tsx
@@ -12,21 +12,18 @@ export default function ProgressBar({
   className?: string;
 }) {
   const [progress, setProgress] = useState(0);
-
   const [current, setCurrent] = useState(0);
+
   useEffect(() => {
-    console.log("Setting up carousel API");
     if (!api) return;
 
     setCurrent(api.selectedScrollSnap() + 1);
-    console.log("current blob:", current);
     api.on("select", () => {
       setCurrent(api.selectedScrollSnap() + 1);
-      console.log("Selected on:", current);
     });
   }, [api]);
+
   useEffect(() => {
-    console.log("Restarting progress bar animation");
     setProgress(0);
     const start = Date.now();
     const interval = setInterval(() => {
@@ -35,5 +32,6 @@ export default function ProgressBar({
     }, 50);
     return () => clearInterval(interval);
   }, [current]);
+
   return <Progress value={progress} className={`${className}`} />;
 }
